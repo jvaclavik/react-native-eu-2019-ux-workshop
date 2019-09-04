2e6030517dd71044560c19542049b3ab15f3be82
Jan Václavík <jvaclavik@gmail.com>
2019-09-04 17:22:26 +0200

    Add theming

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   StyleSheet,
   Platform,
@@ -7,17 +7,18 @@ import {
   TextInput,
   TouchableHighlight,
   View,
-  Switch,
-} from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+  Switch
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { withTheme } from "../utils/theming";
 
-import ScreensToggleIcon from '../components/ScreensToggleIcon';
+import ScreensToggleIcon from "../components/ScreensToggleIcon";
 
 class Login extends React.Component {
   state = {
-    loginValue: '',
-    passwordValue: '',
-    rememberPasswordChecked: false,
+    loginValue: "",
+    passwordValue: "",
+    rememberPasswordChecked: false
   };
 
   setEmail = loginValue => this.setState({ loginValue });
@@ -26,73 +27,82 @@ class Login extends React.Component {
 
   toggleSwitch = () =>
     this.setState(prevState => ({
-      rememberPasswordChecked: !prevState.rememberPasswordChecked,
+      rememberPasswordChecked: !prevState.rememberPasswordChecked
     }));
 
   render() {
-    const { onLoginScreenToggle } = this.props;
+    const { onLoginScreenToggle, theme } = this.props;
+    const dynamicStyles = styles(theme);
     return (
-      <View style={styles.container}>
+      <View style={dynamicStyles.container}>
         <ScreensToggleIcon
-          color={'#131313'}
+          color={theme.accentColor}
           onPress={onLoginScreenToggle}
           shouldClose
         />
-        <StatusBar barStyle={'dark-content'} />
-        <Text style={styles.header}>Nice to see you!</Text>
-        <View style={styles.inputGroup}>
-          <View style={[styles.row, styles.inputRow]}>
-            <Ionicons name="md-person" size={26} color={'#131313'} />
+        <StatusBar barStyle={"dark-content"} />
+        <Text style={dynamicStyles.header}>Nice to see you!</Text>
+        <View style={dynamicStyles.inputGroup}>
+          <View style={[dynamicStyles.row, dynamicStyles.inputRow]}>
+            <Ionicons
+              name="md-person"
+              size={26}
+              color={theme.primaryTextColor}
+            />
             <TextInput
-              style={styles.input}
+              style={dynamicStyles.input}
               onChangeText={this.setEmail}
               value={this.state.loginValue}
               autoCapitalize="none"
               keyboardType="email-address"
               placeholder="e-mail"
-              placeholderTextColor={'#131313'}
+              placeholderTextColor={theme.primaryTextColor}
             />
           </View>
-          <View style={[styles.row, styles.inputRow]}>
-            <Ionicons name="md-lock" size={26} color={'#131313'} />
+          <View style={[dynamicStyles.row, dynamicStyles.inputRow]}>
+            <Ionicons name="md-lock" size={26} color={theme.primaryTextColor} />
             <TextInput
-              style={styles.input}
+              style={dynamicStyles.input}
               onChangeText={this.setPassword}
               value={this.state.passwordValue}
               returnKeyType="go"
               autoCapitalize="none"
               placeholder="password"
-              placeholderTextColor={'#131313'}
+              placeholderTextColor={theme.primaryTextColor}
               secureTextEntry
             />
           </View>
-          <View style={styles.row}>
+          <View style={dynamicStyles.row}>
             <Switch
               value={this.state.rememberPasswordChecked}
               onValueChange={this.toggleSwitch}
               trackColor={{
-                false: '#f5f9ff',
-                true: '#3903fc',
+                false: theme.secondaryBackgroundColor,
+                true: theme.accentColor
               }}
             />
-            <Text style={styles.toggleLabel}>Remember me</Text>
+            <Text style={dynamicStyles.toggleLabel}>Remember me</Text>
           </View>
-          <View style={styles.row}>
+          <View style={dynamicStyles.row}>
             <Switch
               value={this.props.isDarkMode}
               onValueChange={this.props.onToggleDarkMode}
               trackColor={{
-                false: '#f5f9ff',
-                true: '#3903fc',
+                false: theme.secondaryBackgroundColor,
+                true: theme.accentColor
               }}
             />
-            <Text style={styles.toggleLabel}>Dark mode</Text>
+            <Text style={dynamicStyles.toggleLabel}>Dark mode</Text>
           </View>
         </View>
-        <TouchableHighlight style={styles.button}>
-          <View style={styles.row}>
-            <Text style={styles.buttonText}>Login</Text>
-            <Ionicons name="md-arrow-forward" size={16} color={'#FFF'} />
+        <TouchableHighlight style={dynamicStyles.button}>
+          <View style={dynamicStyles.row}>
+            <Text style={dynamicStyles.buttonText}>Login</Text>
+            <Ionicons
+              name="md-arrow-forward"
+              size={16}
+              color={theme.secondaryBackgroundColor}
+            />
           </View>
         </TouchableHighlight>
       </View>
@@ -100,64 +110,64 @@ class Login extends React.Component {
   }
 }
 
-export default Login;
+export default withTheme(Login);
 
-const styles = StyleSheet.create({
+const styles = theme => ({
   container: {
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     right: 0,
     bottom: 0,
     left: 0,
     zIndex: 999999,
-    paddingTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight,
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === "ios" ? 0 : StatusBar.currentHeight,
+    alignItems: "center",
+    justifyContent: "center",
     paddingHorizontal: 10,
-    backgroundColor: '#FFF',
-    elevation: 18,
+    backgroundColor: theme.backgroundColor,
+    elevation: 18
   },
   header: {
     fontSize: 20,
-    fontWeight: 'bold',
-    color: '#131313',
+    fontWeight: "bold",
+    color: theme.primaryTextColor
   },
   inputGroup: {
-    alignItems: 'stretch',
-    width: '100%',
-    marginBottom: 20,
+    alignItems: "stretch",
+    width: "100%",
+    marginBottom: 20
   },
   row: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    margin: 10,
+    flexDirection: "row",
+    alignItems: "center",
+    margin: 10
   },
   inputRow: {
     borderBottomWidth: 2,
-    borderBottomColor: '#131313',
-    justifyContent: 'space-between',
+    borderBottomColor: theme.primaryTextColor,
+    justifyContent: "space-between"
   },
   input: {
     height: 40,
     flex: 0.9,
     fontSize: 16,
-    color: '#131313',
+    color: theme.primaryTextColor,
     paddingVertical: 10,
-    textAlign: 'left',
+    textAlign: "left"
   },
   button: {
-    backgroundColor: '#3903fc',
+    backgroundColor: theme.accentColor,
     borderRadius: 25,
-    padding: 5,
+    padding: 5
   },
   buttonText: {
-    color: '#FFF',
+    color: theme.secondaryBackgroundColor,
     fontSize: 16,
-    textTransform: 'uppercase',
-    marginRight: 10,
+    textTransform: "uppercase",
+    marginRight: 10
   },
   toggleLabel: {
     marginLeft: 10,
-    color: '#131313',
-  },
+    color: theme.primaryTextColor
+  }
 });
--- src/utils/theming.js
+++ src/utils/theming.js
@@ -1,17 +1,23 @@
+import { createTheming } from "@callstack/react-theme-provider";
+
 export const darkTheme = {
-  name: 'dark',
-  backgroundColor: '#131313',
-  secondaryBackgroundColor: '#0c0c0c',
-  primaryTextColor: '#FFF',
-  secondaryTextColor: '#757575',
-  accentColor: '#F8F32B',
+  name: "dark",
+  backgroundColor: "#131313",
+  secondaryBackgroundColor: "#0c0c0c",
+  primaryTextColor: "#FFF",
+  secondaryTextColor: "#757575",
+  accentColor: "#F8F32B"
 };
 
 export const lightTheme = {
-  name: 'light',
-  backgroundColor: '#FFF',
-  secondaryBackgroundColor: '#f5f9ff',
-  primaryTextColor: '#131313',
-  secondaryTextColor: '#333',
-  accentColor: '#3903fc',
+  name: "light",
+  backgroundColor: "#FFF",
+  secondaryBackgroundColor: "#f5f9ff",
+  primaryTextColor: "#131313",
+  secondaryTextColor: "#333",
+  accentColor: "#3903fc"
 };
+
+const { ThemeProvider, withTheme } = createTheming(darkTheme);
+
+export { ThemeProvider, withTheme };
